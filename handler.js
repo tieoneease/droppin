53b0497a855f906a6fa194d64bb2fbbcdcf46e55
Sam Chung <tieoneease@gmail.com>
2016-10-03 03:02:04 -0400

    npm script

--- handler.js
+++ handler.js
@@ -3,6 +3,7 @@
 // Your first function handler
 module.exports.hello = (event, context, cb) => {
   console.log('Is this working?');
+  cb(null, {message: 'lol'});
 };
 
 // You can add more handlers here, and reference them in serverless.yml
