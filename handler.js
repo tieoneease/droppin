8f4e0b43c8594a4d670f1d850cea1fa56dcd401c
Sam Chung <tieoneease@gmail.com>
2016-10-03 02:36:25 -0400

    test travis

--- handler.js
+++ handler.js
@@ -2,7 +2,7 @@
 
 // Your first function handler
 module.exports.hello = (event, context, cb) => {
-  console.log(process.env);
+  console.log('env: ', process.env);
 };
 
 // You can add more handlers here, and reference them in serverless.yml
